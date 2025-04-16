0e74c7cee6832e3f8cd9fb47bcfc572b41050b32
ngoctu020401 <ngoctu020401@gmail.com>
2025-04-16 18:29:32 +0700

    Thêm chức năng thêm và list

---
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+        try {
+            //code...
+            $categories = Category::paginate(10);
+            return response()->json($categories, 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'message' => 'Lỗi',
+                'errors' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    // Chức năng thêm danh mục
+    public function store(Request $request)
+    {
+        //
+        try {
+            //code...
+            $data = $request->validate([
+                'name' => 'required|string'
+            ]);
+            $category = Category::create($data);
+            return response()->json([
+                'message' => 'Bạn đã thêm danh mục thành công',
+                'data' => $category
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'message' => 'Lỗi',
+                'errors' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
