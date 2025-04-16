7d524158af8e777d32741eeba2227d31af092e6a
ngoctu020401 <ngoctu020401@gmail.com>
2025-04-16 19:52:14 +0700

    Tạo route api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\CategoryController;
+use App\Http\Controllers\Api\Admin\ColorController;
+use App\Http\Controllers\Api\Admin\SizeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix('admin')->group(function () {
+    // Kích thước (Size)
+    Route::get('sizes', [SizeController::class, 'index']);
+    Route::post('sizes', [SizeController::class, 'store']);
+    Route::get('sizes/{id}', [SizeController::class, 'show']);
+    Route::put('sizes/{id}', [SizeController::class, 'update']);
+    Route::delete('sizes/{id}', [SizeController::class, 'destroy']);
+
+    // Màu sắc (Color)
+    Route::get('colors', [ColorController::class, 'index']);
+    Route::post('colors', [ColorController::class, 'store']);
+    Route::get('colors/{id}', [ColorController::class, 'show']);
+    Route::put('colors/{id}', [ColorController::class, 'update']);
+    Route::delete('colors/{id}', [ColorController::class, 'destroy']);
+
+    // Danh mục (Category)
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::put('categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
 });
