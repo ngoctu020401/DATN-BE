e55388d1d226a0282c76694b2b839a0528b4bfc7
ngoctu020401 <ngoctu020401@gmail.com>
2025-04-20 23:21:52 +0700

    Update seeder status

--- database/migrations/2025_04_15_181316_create_order_statuses_table.php
+++ database/migrations/2025_04_15_181316_create_order_statuses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('order_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->json('next_status');
+            $table->json('next_status')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2025_04_15_181316_create_payment_statuses_table.php
+++ database/migrations/2025_04_15_181316_create_payment_statuses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('payment_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->json('next_status');
+            $table->json('next_status')->nullable();
             $table->timestamps();
         });
     }
