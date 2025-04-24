3d093ade449658eeac1db1be104386e5c437f1b5
ngoctu020401 <ngoctu020401@gmail.com>
2025-04-24 15:21:19 +0700

    Fix

--- app/Http/Controllers/Api/User/HomeController.php
+++ app/Http/Controllers/Api/User/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
     public function allProduct(Request $request)
     {
         $query = Product::with(['category', 'variationMinPrice']);
-        $query = $this->filterProductQuery($query, $request);
+
 
         $listProducts = $query->get();
         return response()->json($listProducts, 200);
@@ -47,51 +47,9 @@ class HomeController extends Controller
 
         return response()->json($products, 200);
     }
-    //
-    protected function filterProductQuery($query, Request $request)
-    {
-        if ($request->filled('keyword')) {
-            $query->where('name', 'like', '%' . $request->keyword . '%');
-        }
-
-        if ($request->filled('category_id')) {
-            $query->where('category_id', $request->category_id);
-        }
-
-        if ($request->filled('price_from')) {
-            $query->whereHas('variationMinPrice', function ($q) use ($request) {
-                $q->where('price', '>=', $request->price_from);
-            });
-        }
-
-        if ($request->filled('price_to')) {
-            $query->whereHas('variationMinPrice', function ($q) use ($request) {
-                $q->where('price', '<=', $request->price_to);
-            });
-        }
-
-        if ($request->filled('sort')) {
-            switch ($request->sort) {
-                case 'price_asc':
-                    $query->withMin('variationMinPrice', 'price')->orderBy('variation_min_price_price');
-                    break;
-                case 'price_desc':
-                    $query->withMin('variationMinPrice', 'price')->orderByDesc('variation_min_price_price');
-                    break;
-                default:
-                    $query->orderBy('created_at', 'desc');
-            }
-        } else {
-            $query->orderBy('created_at', 'desc');
-        }
-
-        return $query;
-    }
     public function productByCategory(Request $request, $id)
     {
         $query = Product::with(['category', 'variationMinPrice'])->where('category_id', $id);
-        $query = $this->filterProductQuery($query, $request);
-
         $listProducts = $query->get();
         return response()->json($listProducts, 200);
     }
